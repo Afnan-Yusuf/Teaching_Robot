d44063e98ad8462a77e66173b1124e6c2287b4ad
sumamashuhaib <sumamashuhaib2@gmail.com>
2024-10-09 15:38:16 +0530

    serialcommands set

--- include/arduinointerface.h
+++ include/arduinointerface.h
@@ -3,11 +3,20 @@
 #define RXD2 16
 #define TXD2 17
 
+int mode = 0;// 0 = walking; 1 = handshake; 2 = stop;
+
 void initserial2()
 {
     Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
 }
 
 void arduinointerface(){
+    if(mode == 0){
+        Serial2.println('w');
+    }else if(mode == 1){
+        Serial2.println('h');
+    }else if(mode == 2){
+        Serial2.println('s');
+    }
     Serial2.println("HI");
 }
