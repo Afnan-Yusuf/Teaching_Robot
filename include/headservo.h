2c6118ebd79793840fa78f3552cb6524d0f9a7e2
sumamashuhaib <sumamashuhaib2@gmail.com>
2024-10-09 17:21:14 +0530

    servo modified

--- include/headservo.h
+++ include/headservo.h
@@ -6,8 +6,10 @@ Servo myservo;
 int pos = 0; 
 int servoPin = 4;
 int increment = 1;          // Increment value for servo position
- 
 const long interval = 20;   // Update interval for smooth movement (20ms)
+unsigned long pmn1 = 0; // Store the last time the servo was updated
+enum SweepDirection { SWEEPING_UP, SWEEPING_DOWN };
+SweepDirection currentDirection = SWEEPING_UP; // Direction for sweeping
 
 void headservoinit() {
     // Allow allocation of all timers
@@ -20,13 +22,29 @@ void headservoinit() {
 }
 
 void headservoSweep() {
-    //normal servo sweep code
-    for (pos = 0; pos <= 180; pos += increment) { // goes from 0 degrees to 180 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
-    for (pos = 180; pos >= 0; pos -= increment) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
+    unsigned long currentMillis = millis();
+
+    // Check if it's time to update the servo position
+    if (currentMillis - pmn1 >= interval) {
+        pmn1 = currentMillis; // Save the last update time
+
+        // Update the servo position based on the current direction
+        if (currentDirection == SWEEPING_UP) {
+            pos += increment; // Increase position
+            if (pos >= 180) { // Check if we've reached the max position
+                pos = 180;
+                currentDirection = SWEEPING_DOWN; // Change direction
+            }
+        } else { // SWEEPING_DOWN
+            pos -= increment; // Decrease position
+            if (pos <= 0) { // Check if we've reached the min position
+                pos = 0;
+                currentDirection = SWEEPING_UP; // Change direction
+            }
+        }
+
+        myservo.write(pos); // Update servo position
     }
 }
+
+
