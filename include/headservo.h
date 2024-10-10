3b3ee4dd72f484eb6a0ee7fcf2aca096e1643815
sumamashuhaib <sumamashuhaib2@gmail.com>
2024-10-10 17:55:59 +0530

    fixed servo and ct

--- include/headservo.h
+++ include/headservo.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <Ramp.h>
+//#include "ct6b.h"
+
+extern int z;
+
+ramp servoramp;
+int finalpos;
 
 Servo myservo;
 int pos = 0; 
@@ -10,7 +17,7 @@ const long interval = 20;   // Update interval for smooth movement (20ms)
 unsigned long pmn1 = 0; // Store the last time the servo was updated
 enum SweepDirection { SWEEPING_UP, SWEEPING_DOWN };
 SweepDirection currentDirection = SWEEPING_UP; // Direction for sweeping
-
+void writeservo(int angle);
 void headservoinit() {
     // Allow allocation of all timers
     ESP32PWM::allocateTimer(0);
@@ -22,29 +29,14 @@ void headservoinit() {
 }
 
 void headservoSweep() {
-    unsigned long currentMillis = millis();
+    myservo.write(z);
+}
 
-    // Check if it's time to update the servo position
-    if (currentMillis - pmn1 >= interval) {
-        pmn1 = currentMillis; // Save the last update time
+void writeservo(int angle){
+    finalpos = myramp.update();
+    myramp.go(angle, 500, LINEAR, ONCEFORWARD);
+    myservo.write(finalpos);
 
-        // Update the servo position based on the current direction
-        if (currentDirection == SWEEPING_UP) {
-            pos += increment; // Increase position
-            if (pos >= 180) { // Check if we've reached the max position
-                pos = 180;
-                currentDirection = SWEEPING_DOWN; // Change direction
-            }
-        } else { // SWEEPING_DOWN
-            pos -= increment; // Decrease position
-            if (pos <= 0) { // Check if we've reached the min position
-                pos = 0;
-                currentDirection = SWEEPING_UP; // Change direction
-            }
-        }
-
-        myservo.write(pos); // Update servo position
-    }
 }
 
 
